39a0d10afaebceb334a17bf5de77efbc94728b82
Nativeborn <nativeborncitizen@gmail.com>
2013-10-16 00:31:01 +0300

    Рефакторинг функции stem()

--- stemmer.py
+++ stemmer.py
@@ -3,20 +3,25 @@ import sys
 
 from config import DIC_FILE, AFF_FILE
 
+try:
+    from hunspell import HunSpell
+except ImportError:
+    sys.stderr.write("error: %s\n" % "Can't import hunspell module!!!")
+    sys.exit(1)
+
 
 def stem(word):
-    try:
-        import hunspell
-    except ImportError:
-        sys.stderr.write("error: %s\n" % "Can't import hunspell module!!!")
+    hunspell_object = HunSpell(DIC_FILE, AFF_FILE)
+    stemmed_list = hunspell_object.stem(word)
+    if len(stemmed_list) > 0:
+        return stemmed_list[0]
+    else:
         sys.exit(1)
-
-    hunspell_object = hunspell.HunSpell(DIC_FILE, AFF_FILE)
-    return hunspell_object.stem(word)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 2:
-        stemmed_list = stem(sys.argv[1])
-        if len(stemmed_list) > 0:
-            print stemmed_list[0]
+
+    if len(sys.argv) != 2:
+        sys.exit(1)
+
+    print stem(sys.argv[1])
